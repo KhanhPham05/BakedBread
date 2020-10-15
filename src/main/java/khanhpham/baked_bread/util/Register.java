5ba9466f2faf522abf188119545bcae4a46f22f3
KhanhPham05 <68932626+KhanhPham05@users.noreply.github.com>
2020-10-15 11:13:14 +0700

    add more stuff

--- src/main/java/khanhpham/baked_bread/util/Register.java
+++ src/main/java/khanhpham/baked_bread/util/Register.java
@@ -17,6 +17,7 @@ public class Register {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    public static final RegistryObject<Item> COOKED_BREAD = ITEMS.register("cooked_bread", () -> new Item(new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().hunger(8).saturation(3.5f).build())));
-
+    public static final RegistryObject<Item> COOKED_BREAD = ITEMS.register("cooked_bread", () -> new Item(new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().hunger(8).saturation(0.7f).build())));
+    public static final RegistryObject<Item> BREAD_WITH_BEEF = ITEMS.register("baked_bread_with_meat", () -> new Item(new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().hunger(10).saturation(0.8f).build())));
+    public static final RegistryObject<Item> KNIFE = ITEMS.register("knife", () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
 }
